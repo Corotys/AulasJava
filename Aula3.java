63012cd62e35a70452e79f000d8aaf6814794d52
Corotys <mateus_felipe232@hotmail.com>
2024-09-04 20:03:07 +0000

    Aula 03s

--- Aula3.java
+++ Aula3.java
@@ -1,9 +1,39 @@
+import java.util.Scanner;
+
 /**
  * Aula3
- */
+ */ 
+
 public class Aula3 {
 
     public static void main(String[] args) {
-        
+    Scanner leitorTxt = new Scanner(System.in);
+    System.out.println("digite seu nome 1:\n");
+    String nome = leitorTxt.nextLine();
+    System.out.println(nome);
+    
+    System.out.println("digite seu nome 2:\n");
+    nome = leitorTxt.nextLine();
+    System.out.println(nome);
+
+    System.out.println("digite seu nome 3:\n");
+    nome = leitorTxt.nextLine();
+    System.out.println(nome);
+
+    System.out.println("digite seu nome 4:\n");
+    nome = leitorTxt.nextLine();
+    System.out.println(nome);
+
+    System.out.println("digite seu nome 5:\n");
+    nome = leitorTxt.nextLine();
+    System.out.println(nome);
+
+    leitorTxt.close();
+
     }
-}
+    
+
+    
+    }
+
+    
